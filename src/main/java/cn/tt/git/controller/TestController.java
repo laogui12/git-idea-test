51d63b75ea5294b3fc82c6522ae06d9ebc0a754a
cms <tt_cms@163.com>
2022-03-31 16:34:06 +0800

    dev第一次提交

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.tt.git.controller;
 public class TestController {
         public static void main(){
             System.out.println("hello git");
+            System.out.println("dev");
         }
 }
